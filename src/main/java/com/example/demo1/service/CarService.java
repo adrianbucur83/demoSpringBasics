70566b9a0ec2fecf506d0adcfb74891e81712f4a
Adrian Bucur <adrianbucur83@gmail.com>
2024-03-25 21:05:40 +0200

    final stuff

--- src/main/java/com/example/demo1/service/CarService.java
+++ src/main/java/com/example/demo1/service/CarService.java
@@ -11,30 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class CarService {
-
-    private final CarValidator carValidator;
-    List<Car> cars = new ArrayList<>();
-
-    public CarService(CarValidator carValidator) {
-        this.carValidator = carValidator;
-    }
-
-    public String getAllCars() {
-        StringBuilder output = new StringBuilder();
-
-        Car mercedes = new Car("", new DieselEngine());
-        if (carValidator.validateCar(mercedes)) {
-            cars.add(mercedes);
-        }
-        Car daciaSpring = new Car("Dacia Spring", new ElectricEngine());
-        Car loganPetrol = new Car("Logan Petrol", new PetrolEngine());
-        cars.add(daciaSpring);
-        cars.add(loganPetrol);
-        for (Car car : cars) {
-            output.append(car.getModel()).append(" ").append(car.startEngine()).append("<br>");
-        }
-        return output.toString();
-    }
-
+public interface CarService {
+    public String getAllCars() ;
 }
---
+++ src/main/java/com/example/demo1/service/impl/CarServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.demo1.service.impl;
+
+import com.example.demo1.model.Car;
+import com.example.demo1.model.impl.DieselEngine;
+import com.example.demo1.model.impl.ElectricEngine;
+import com.example.demo1.model.impl.PetrolEngine;
+import com.example.demo1.util.CarValidator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarServiceImpl {
+    private final CarValidator carValidator;
+    List<Car> cars = new ArrayList<>();
+
+    public CarServiceImpl(CarValidator carValidator) {
+        this.carValidator = carValidator;
+    }
+    public String getAllCars() {
+        StringBuilder output = new StringBuilder();
+
+        Car mercedes = new Car("", new DieselEngine());
+        if (carValidator.validateCar(mercedes)) {
+            cars.add(mercedes);
+        }
+        Car daciaSpring = new Car("Dacia Spring", new ElectricEngine());
+        Car loganPetrol = new Car("Logan Petrol", new PetrolEngine());
+        cars.add(daciaSpring);
+        cars.add(loganPetrol);
+        for (Car car : cars) {
+            output.append(car.getModel()).append(" ").append(car.startEngine()).append("<br>");
+        }
+        return output.toString();
+    }
+}
