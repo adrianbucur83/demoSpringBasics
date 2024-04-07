1c43066d0919dad87fe02287bea6917d8c819591
Adriana <acronmar1@gmail.com>
2024-04-07 17:27:14 +0300

    controler and views created

---
+++ src/main/java/com/example/demo1/controller/ProductReviewsController.java
@@ -0,0 +1,59 @@
+package com.example.demo1.controller;
+
+import com.example.demo1.model.WelcomeMessage;
+import com.example.demo1.model.Review;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/reviews")
+public class ProductReviewsController {
+
+
+
+    //CRUD
+    @GetMapping(" /review-form")
+    public String displayReviewForm(Model model) {
+        model.addAttribute("review", new Review());
+
+        return "reviews/review-form";
+    }
+
+    ;
+
+    @PostMapping("/your-review")
+    public String createReviews(@RequestParam(required = false) @ModelAttribute Review review,
+                                Model model) {
+        model.addAttribute("review", review);
+
+        return "reviews/create-reviews";
+    }
+
+    ;
+
+    @GetMapping("/all-reviews")
+    public String getReviews() {
+        return "get-reviews";
+    }
+
+    ;
+
+    @PutMapping("/update-review/{id}")
+    public String updateReviews() {
+        return "update-reviews";
+    }
+
+    ;
+
+    @DeleteMapping("delete-review/{id}")
+    public String deleteReviews() {
+        return "delete-review";
+    }
+
+    ;
+}
---
+++ src/main/java/com/example/demo1/model/Review.java
@@ -0,0 +1,47 @@
+package com.example.demo1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Date;
+
+public class Review {
+ //   private int id;
+    private String reviewMsg;
+   // private Date date;
+    private String userName;
+
+    public Review() {
+
+    }
+
+    public String getReviewMsg() {
+        return reviewMsg;
+    }
+
+    public void setReviewMsg(String reviewMsg) {
+        this.reviewMsg = reviewMsg;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Review(String reviewMsg, String userName) {
+        this.reviewMsg = reviewMsg;
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "reviewMsg='" + reviewMsg + '\'' +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
+}
