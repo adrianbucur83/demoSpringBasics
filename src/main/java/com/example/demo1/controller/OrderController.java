3613c7ea0e868eb1b76992ed1869a8b175bb4946
Denis Bulat <dennis.bulat@gmail.com>
2024-04-11 09:17:05 +0200

    OrderRepoServiceController

--- src/main/java/com/example/demo1/controller/OrderController.java
+++ src/main/java/com/example/demo1/controller/OrderController.java
@@ -1,125 +1,69 @@
 package com.example.demo1.controller;
 
 import com.example.demo1.model.Order;
-import com.example.demo1.model.WelcomeMessage;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo1.service.OrderService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
-import java.util.Iterator;
+
 import java.util.List;
 
 @Controller
 @RequestMapping("/orders")
+@AllArgsConstructor
 public class OrderController {
 
-    @Autowired
-    WelcomeMessage welcomeMessage;
-    List<Order> orders = new ArrayList<>();
-     {
-        Order order1 = new Order(1l, "Denis Bulat", 100l);
-        Order order2 = new Order(2l, "Natalia Dolghieru", 100l);
-        orders.add(order1);
-        orders.add(order2);
-    }
+    private OrderService orderService;
 
-    public Order createOrder(Order order){
-        order.setOrderId(order.getOrderId());
-        order.setCustomerName(order.getCustomerName());
-        order.setTotalAmount(order.getTotalAmount());
-//        order.setNotes(order.getNotes());
-//        order.setOrderStatus(order.getOrderStatus());
-//        order.setDateTime(order.getDateTime());
-//        order.setBillingAddress(order.getBillingAddress());
-//        order.setPaymentMethod(order.getPaymentMethod());
-//        order.setPaymentStatus(order.getPaymentStatus());
-//        order.setShippingAddress(order.getShippingAddress());
-//        order.setShippingStatus(order.getShippingStatus());
-     return order;
-     }
-    public Order updateOrder(Order order){
-
-        order.setOrderId(order.getOrderId());
-        order.setCustomerName(order.getCustomerName());
-        order.setTotalAmount(order.getTotalAmount());
-
-        return order;
-    }
     @GetMapping
-    public String createOrder(Model model){
+    public String getOrders(Model model){
+        List<Order> orders = orderService.getAllOrders();
         model.addAttribute("orders", orders);
         return "orders/order";
     }
+
     @GetMapping("/new")
-    public String getOrder(Model model){
+    public String createOrder(Model model){
         Order order = new Order();
         model.addAttribute("order", order);
         return "orders/create-order";
     }
+
     @PostMapping
-    public String createOrders(@ModelAttribute("order") Order order){
-        createOrder(order);
-        orders.add(order);
+    public String showOrders(@ModelAttribute("order") Order order){
+        orderService.createOrder(order);
         return "redirect:/orders";
     }
+
     @GetMapping("/{orderId}/edit")
     public String editOrder(@PathVariable("orderId") Long orderId , Model model) {
-//        Order order = orders.stream()
-//                            .filter(o -> o.getOrderId().equals(orderId))
-//                            .findFirst()
-//                            .orElse(null);
-//        model.addAttribute("order", order);
-        Order orderToEdit = null;
-        for (Order order : orders) {
-            if (order.getOrderId().equals(orderId)) {
-                orderToEdit = order;
-                break; // Exit loop once the desired order is found
-            }
-        }
 
-        if (orderToEdit != null) {
-            model.addAttribute("order", orderToEdit);
-        }
+        Order order = orderService.getOrderById(orderId);
+
+        model.addAttribute("order", order);
+
         return "orders/edit-order";
-
     }
+
     @PostMapping("/{orderId}")
     public String updateOrders(@PathVariable Long orderId,
                                @ModelAttribute("order") Order order){
-        for (Order order1 : orders){
-            if(order1.getOrderId().equals(orderId)){
-                order1.setOrderId(order.getOrderId());
-                order1.setTotalAmount(order.getTotalAmount());
-                order1.setCustomerName(order.getCustomerName());
-            }
-        }
+        order.setOrderId(orderId);
+        orderService.updateOrder(order);
         return "redirect:/orders";
     }
 
     @GetMapping("/{orderId}/delete")
     public String deleteOrder(@PathVariable("orderId")Long orderId) {
-        Iterator<Order> iterator = orders.iterator();
-        while (iterator.hasNext()){
-            Order order1 = iterator.next();
-            if (order1.getOrderId().equals(orderId)){
-                iterator.remove();
-                break;
-            }
-        }
+        orderService.deleteOrder(orderId);
         return "redirect:/orders";
     }
     @GetMapping("/{orderId}/view")
     public String viewOrder(@PathVariable("orderId")Long orderId, Model model) {
 
-         Order orderToView = null;
-         for (Order order: orders){
-             if (order.getOrderId().equals(orderId)){
-                 orderToView = order;
-                 break;
-             }
-         }
-         model.addAttribute("order", orderToView);
+        Order orderToView = orderService.getOrderById(orderId);
+        model.addAttribute("order", orderToView);
 
         return "orders/view-order";
     }
