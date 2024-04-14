e5619bc914f6ab9f4e13aa2fbe651d0f64a6bf9d
VladIulian96 <ramez_vlad69@yahoo.com>
2024-04-14 20:19:29 +0300

    Added a product repository

---
+++ src/main/java/com/example/demo1/repository/ProductsRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo1.repository;
+
+import com.example.demo1.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductsRepository extends JpaRepository<Product, Integer> {
+
+
+
+}
