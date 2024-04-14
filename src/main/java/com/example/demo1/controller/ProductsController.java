78d22f9b7bb5eba7a309f41d3245c617970471ca
VladIulian96 <ramez_vlad69@yahoo.com>
2024-04-14 19:33:43 +0300

    Fixed a few things and added database connection

--- src/main/java/com/example/demo1/controller/ProductsController.java
+++ src/main/java/com/example/demo1/controller/ProductsController.java
@@ -3,6 +3,7 @@ package com.example.demo1.controller;
 import com.example.demo1.model.Product;
 import com.example.demo1.service.impl.ProductService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +11,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequiredArgsConstructor
 public class ProductsController {
 
     @Autowired
-    ProductService productService;
+    private final ProductService productService;
 
     @GetMapping(value = "/products")
     public String listAllProducts(Model model) {
@@ -41,11 +43,11 @@ public class ProductsController {
         if(productService.productExists(productId)) {
             Product oldProduct = productService.getProductById(productId);
 
-            oldProduct.setId(newProduct.getId());
-            oldProduct.setName(newProduct.getName());
-            oldProduct.setPrice(newProduct.getPrice());
-            oldProduct.setStock(newProduct.getStock());
-            oldProduct.setDescription(newProduct.getDescription());
+//            oldProduct.setId(newProduct.getId());
+//            oldProduct.setName(newProduct.getName());
+//            oldProduct.setPrice(newProduct.getPrice());
+//            oldProduct.setStock(newProduct.getStock());
+//            oldProduct.setDescription(newProduct.getDescription());
 
             productService.updateProduct(oldProduct, newProduct);
             redirectAttributes.addFlashAttribute("updateMessage", "Successful update!");
